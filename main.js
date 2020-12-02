9d7e984d8188fdaeb7e1614ef9fc19d9b07be468
codeinngincode <66986522+codeinngincode@users.noreply.github.com>
2020-12-02 19:50:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,105 +1,38 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
 
-nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"];
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
 
-random_number = Math.floor(Math.random() * 4);
-console.log(random_number);
-rover_width = 100;
-rover_height = 90;
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
 
-background_image = nasa_mars_images_array[random_number];
-console.log("background_image = " + background_image);
-rover_image = "rover.png";
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
 
-rover_x = 10;
-rover_y = 10;
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(450, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
 
-function add() {
-	background_imgTag = new Image(); //defining a variable with a new image
-	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
-	background_imgTag.src = background_image;   // load image
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
 
-	rover_imgTag = new Image(); //defining a variable with a new image
-	rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
-	rover_imgTag.src = rover_image;   // load image
-}
-
-function uploadBackground() {
-	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-}
-
-function uploadrover() {
-	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
-}
-
-
-window.addEventListener("keydown", my_keydown);
-
-function my_keydown(e)
-{
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			up();
-			console.log("up");
-		}
-		if(keyPressed == '40')
-		{
-			down();
-			console.log("down");
-		}
-		if(keyPressed == '37')
-		{
-			left();
-			console.log("left");
-		}
-		if(keyPressed == '39')
-		{
-			right();
-			console.log("right");
-		}
-}
-
-function up()
-{
-	if(rover_y >=0)
-	{
-		rover_y = rover_y - 10;
-		console.log("When up arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
-		 uploadBackground();
-		 uploadrover();
-	}
-}
-function down()
-{
-	if(rover_y <=500)
-	{
-		rover_y =rover_y+ 10;
-		console.log("When down arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
-		uploadBackground();
-		 uploadrover();
-	}
-}
-function left()
-{
-	if(rover_x >= 0)
-	{
-		rover_x =rover_x - 10;
-		console.log("When left arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
-		uploadBackground();
-		 uploadrover();
-	}
-}
-function right()
-{
-	if(rover_x <= 700)
-	{
-		rover_x =rover_x + 10;
-		console.log("When right arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
-		uploadBackground();
-		uploadrover();
-   }
-}
-	
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
