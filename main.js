e5ce48655810aaf3c4fa63c966a55b6c7200468f
codeinngincode <66986522+codeinngincode@users.noreply.github.com>
2020-12-04 07:18:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,231 @@
-var mouseEvent = "empty";
+const { fabric } = require("./fabric");
+
+var canvas =new fabric.canvas("mycanvas");
+
+playerx=10
+
+playery=10
+
+Blockimagewidth=30
+
+Blockimageheight=30
+
+var playerobject="";
+
+var blockobject="";
+
+function playerupdate()
+{
+
+    fabric.Image.fromURL("player.png",function(Img){
+
+        playerobject=Img;
+        playerobject.scaleToWidth(150);
+        playerobject.scaleToHeight(140);
+        playerobject.set({
+
+            top:playery,left:playerx
+
+        });
+
+        canvas.add(playerobject);
+
+    })
+
+}
+
+function new_image(get_image)
+{
+
+    fabric.Image.fromURL(get_image,function(Img){
+
+        blockobject=Img;
+        blockobject.scaleToWidth(Blockimagewidth);
+        blockobject.scaleToHeight(Blockimageheight);
+        blockobject.set({
+
+            top:playery,left:playerx
+
+        });
+
+        canvas.add(blockobject);
+
+    })
+
+}
+
+window.addEventListener("keydown",my_keydown);
 
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-    canvas.addEventListener("mousedown", my_mousedown);
-    
-    function my_mousedown(e)
-    {
+function my_keydown(e) 
+{
 
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        radius = document.getElementById("radius").value;
-        mouseEvent = "mouseDown";
+    keyPressed=e.keycode;
+
+    console.log(keyPressed);
+
+    if (e.shiftkey == true && keyPressed == "80") {
+        
+        Blockimagewidth=Blockimagewidth+10;
+
+        Blockimageheight=Blockimageheight+10;
+
+        document.getElementById("current_width").innerHTML=Blockimagewidth;
+
+        document.getElementById("current_height").innerHTML=Blockimageheight;
+
     }
 
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    if (e.shiftkey && keyPressed == "77") {
+        
+        Blockimagewidth=Blockimagewidth-10;
 
-        if (mouseEvent == "mouseDown") {
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
-        ctx.stroke();
+        Blockimageheight=Blockimageheight-10;
+
+        document.getElementById("current_width").innerHTML=Blockimagewidth;
+
+        document.getElementById("current_height").innerHTML=Blockimageheight;
+
+    }
+    
+    if (keyPressed == "38") {
+        
+        up();
+        
+        console.log("up")
+
+    }
+
+    if (keyPressed == "40") {
+        
+        down();
+
+        console.log("down")
+
+    }
+
+    if (keyPressed == "37") {
+        
+        left();
+
+        console.log("left")
+
+    }
+
+    if (keyPressed == "39") {
+        
+        right();
+
+        console.log("right")
+
+    }
+
+    if (keyPressed == "87") {
+        
+        new_image("wall.jpg");
+
+        console.log("up")
+
+    }
+
+    if (keyPressed == "71") {
+        
+        new_image("ground.png");
+
+    }
+
+    if (keyPressed == "76") {
+        
+        new_image("light_green.png");
+
+    }
+
+    if (keyPressed == "84") {
+        
+        new_image("trunk.jpg");
+
+    }
+
+    if (keyPressed == "82") {
+        
+        new_image("roof.jpg");
+
+    }
+
+    if (keyPressed == "89") {
+        
+        new_image("yellow_wall.png");
+
+    }
+
+    if (keyPressed == "68") {
+        
+        new_image("dark_green.png");
+
+    }
+
+    if (keyPressed == "85") {
+        
+        new_image("unique.png");
+
+    }
+
+    if (keyPressed == "67") {
+        
+        new_image("cloud.jpg");
+
+    }
+    
+}
+
+function up() {
+    
+    if (playery >= 0) {
+
+        playery=playery-Blockimageheight;
+
+        canvas.remove(playerobject);
+
+        playerupdate();
+        
+    }
+
+}
+
+function down() {
+    
+    if (playery <= 500) {
+
+        playery=playery+Blockimageheight;
+
+        canvas.remove(playerobject);
+
+        playerupdate();
+        
+    }
+
+    function left() {
+    
+        if (playerx >= 0) {
+    
+            playerx=playerx-Blockimagewidth;
+    
+            canvas.remove(playerobject);
+    
+            playerupdate();
+            
         }
 
-    }
+        function right() {
+    
+            if (playerx <= 850) {
+        
+                playerx=playerx+Blockimagewidth;
+        
+                canvas.remove(playerobject);
+        
+                playerupdate();
+                
+            }
 
-    canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseEvent = "mouseUP";
-    }
-
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
-
-
-//Additional Activity
-function clearArea() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
